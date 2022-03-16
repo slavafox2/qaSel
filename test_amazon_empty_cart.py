7ccf11aba3228e4ef25bb76f346c762c49be5fc9
Slav <tyop@tut.by>
2022-03-16 10:32:27 +0300

    first

---
+++ test_amazon_empty_cart.py
@@ -0,0 +1,26 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+class TestAmazonCart:
+    driver = None
+
+    def setup_method(self):
+        self.driver = webdriver.Chrome(executable_path=r'C:\Users\SL\PycharmProjects\qaSel\chromedriver.exe')
+
+        self.driver.implicitly_wait(5)
+        self.driver.get('https://www.amazon.com')
+
+
+    def test_empty_cart(self):
+        self.driver.find_element(By.ID, 'nav-cart').click()
+        actual_text = self.driver.find_element(By.XPATH, '//div[@id="sc-active-cart"]//h2').text
+        expected_text = 'Your Amazon Cart is empty'
+
+        assert expected_text == actual_text, f'Expected text: "{expected_text}", but actual text: "{actual_text}"'
+
+
+    def teardown_method(self):
+        self.driver.quit()
---
+++ test_amazon_search.py
@@ -0,0 +1,28 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+class TestAmazon:
+    search_words = {'dress', 'shoes'}
+    driver = None
+
+    def setup_method(self):
+        self.driver = webdriver.Chrome(executable_path=r'C:\Users\SL\PycharmProjects\qaSel\chromedriver.exe')
+
+        self.driver.implicitly_wait(5)
+        self.driver.get('https://www.amazon.com')
+
+    @pytest.mark.parametrize('search_query', search_words)
+    def test_amazon_search(self, search_query):
+        search = self.driver.find_element(By.ID, 'twotabsearchtextbox')
+        search.send_keys(search_query, Keys.ENTER)
+
+        expected_text = f'"{search_query}"'
+        actual_text = self.driver.find_element(By.XPATH, '//span[@class="a-color-state a-text-bold"]').text
+
+        assert expected_text == actual_text, f'Error. Expected text: {expected_text}, but actual text: {actual_text}'
+
+    def teardown_method(self):
+        self.driver.quit()
